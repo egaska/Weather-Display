683caaab700b146f439cc53ba3e243e7a94b0e06
Emily Gaska <emilygaska@gmail.com>
2020-05-04 16:02:19 -0500

    buildQueryURL functioning

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 
 function buildQueryURL() {
-
-    var cityName = "Gardner";
+    cityName = $("#searchinput").val().trim();
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=2aa3937301d9ea4d1d3ffd9639357512";
-    // cityName = $("#search-term").val().trim();
-   
-
-    
     console.log(queryURL);
 
 }
-buildQueryURL();
+ 
+$("#searchButton").on("click", buildQueryURL);
