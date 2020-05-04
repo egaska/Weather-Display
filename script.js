7560006f7d33da34dc4017e415ac23e7b1d6b0b7
Emily Gaska <emilygaska@gmail.com>
2020-05-04 16:32:53 -0500

    Created and tested renderButtons(). Currently creates buttons with no function.

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ function buildQueryURL() {
     cityName = $("#searchinput").val().trim();
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=2aa3937301d9ea4d1d3ffd9639357512";
     console.log(queryURL);
+    renderButtons(cityName);
+    return queryURL;
+}
+
+function pullCityInformation(buildQueryURL){
 
 }
+
+function renderButtons(cityName) {
+    let buttonSearchedCity = $("<button>");
+    buttonSearchedCity.addClass("city-button btn btn-light col-12");
+    buttonSearchedCity.attr("data-name", cityName);
+    buttonSearchedCity.text(cityName);
+    $("#previousSearch").append(buttonSearchedCity);
+}
+
+
  
 $("#searchButton").on("click", buildQueryURL);
