1e1b87d695f087b11ff3396545a78de8450e5cd9
Emily Gaska <emilygaska@gmail.com>
2020-05-04 21:46:59 -0500

    Was able to get the correct paths to call the information from the API.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function pullCityInformation(buildQueryURL) {
 
 }
 
+//Renders the buttons that show previously seached cities. 
 function renderSearchedButtons(cityName) {
     let buttonSearchedCity = $("<button>");
     buttonSearchedCity.addClass("city-button btn btn-light col-12");
@@ -16,8 +17,8 @@ function renderSearchedButtons(cityName) {
 $("#searchButton").on("click", function () {
 
     cityName = $("#searchinput").val().trim();
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-    var queryURL_UV =
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+    // var queryURL_UV = ;
     console.log(queryURL);
     renderSearchedButtons(cityName);
     console.log("Test")
@@ -26,7 +27,22 @@ $("#searchButton").on("click", function () {
         method: "GET"
     })
         .then(function (response) {
-            console.log(response.cod);
+            console.log(response);
+
+
+            // response.data.weather[0].icon
+            var pulledName = response.cityName;
+            var latitude = response.city.coord.lat;
+            var longitude = response.city.coord.lon;
+            var temperature = response.list[0].main.temp;
+            var humidity = response.list[0].main.humidity;
+            var windSpeed = response.list[0].wind.speed;
+
+            console.log (temperature);
+            console.log (humidity);
+            console.log (windSpeed);
+            
+            // var uvIndex =
         })
 
 })
