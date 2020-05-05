b76b177cc8354dc2c7e061d238f2bf1a28a2154a
Emily Gaska <emilygaska@gmail.com>
2020-05-05 07:40:09 -0500

    Added and tested UV Index color settings.

--- script.js
+++ script.js
@@ -14,34 +14,102 @@ function renderSearchedButtons(cityName) {
     $("#previousSearch").append(buttonSearchedCity);
 }
 
+//Function to get the UV Index
+function getUV(lat, lon) {
+    var latitude = lat;
+    var longitude = lon;
+    var uvIndex = "";
+
+       // Creating storing div tag
+       var forecastDiv = $("<div>");
+    var queryURL_UV = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + latitude + "&lon=" + longitude;
+    $.ajax({
+        url: queryURL_UV,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            uvIndex = response.value;
+            console.log("UV Index: " + uvIndex);
+            
+
+
+        if (uvIndex >= 11){
+            var uvIndexEl = $("<p>").html('UV Index: <p class="alert alert-danger" style = "background-color: purple; color: white" >' + uvIndex + '</p>');
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+        }
+        else if( uvIndex >= 8 ){
+            var uvIndexEl = $("<p>").html('UV Index: <p class="alert alert-danger">' + uvIndex + '</p>');
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+        }
+        else if( uvIndex >= 6 ){
+            var uvIndexEl = $("<p>").html('UV Index: <p class="alert alert-warning">' + uvIndex + '</p>');
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+        }
+        else if( uvIndex >= 3){
+            var uvIndexEl = $("<p>").html('UV Index: <p class="alert alert-primary">' + uvIndex + '</p>');
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+        }
+        else{
+            var uvIndexEl = $("<p>").html('UV Index: <p class="alert alert-light">' + uvIndex + '</p>');
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+        }
+            forecastDiv.append(uvIndexEl);
+            $("#forecast").append(forecastDiv);
+
+        })
+        console.log("UV Index: " + uvIndex);
+      
+     
+
+}
+
 $("#searchButton").on("click", function () {
 
     cityName = $("#searchinput").val().trim();
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
-    // var queryURL_UV = ;
-    console.log(queryURL);
     renderSearchedButtons(cityName);
-    console.log("Test")
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
+
             console.log(response);
-
-
             // response.data.weather[0].icon
-            var pulledName = response.cityName;
+            var pulledName = response.city.name;
             var latitude = response.city.coord.lat;
             var longitude = response.city.coord.lon;
             var temperature = response.list[0].main.temp;
             var humidity = response.list[0].main.humidity;
             var windSpeed = response.list[0].wind.speed;
-
-            console.log (temperature);
-            console.log (humidity);
-            console.log (windSpeed);
             
+            // Creating storing div tag
+            var forecastDiv = $("<div>");
+
+            //Creating tags with the city information
+            var cityEl = $("<h2>").text(pulledName);
+            var tempEl = $("<p>").text("Temperature: " + temperature + "°F");
+            var humidEl = $("<p>").text("Humidity: " + humidity + "%");
+            var windEl = $("<p>").text("Wind Speed: " + windSpeed + "MPH");
+          
+
+            //Appending the forecast div
+            forecastDiv.append(cityEl);
+            forecastDiv.append(tempEl);
+            forecastDiv.append(humidEl);
+            forecastDiv.append(windEl);
+            var uvIndex = getUV(latitude, longitude);
+        
+            $("#forecast").append(forecastDiv);
+
+
             // var uvIndex =
         })
 
